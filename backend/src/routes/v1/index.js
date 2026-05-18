86d6dda2ce0e8d3bb0b39822834c0236d3e1b964
Matias Xavier Roveri <matixr2008@gmail.com>
2026-05-17 21:29:58 -0300

    Update index.js

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,11 +1,9 @@
 import { Router } from 'express'
 import authRoutes from './auth.routes.js'
-
+import usuarioRoutes from './usuario.routes.js'
 
 const router = Router()
-
-
 router.use('/auth', authRoutes)
-
+router.use('/usuario', usuarioRoutes)
 
 export default router
