1641ec2aebfe82fee3fd65f9280fa40db304d245
Matias Xavier Roveri <matixr2008@gmail.com>
2026-05-17 22:20:16 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/~ocho/',
   server: {
     port: 4173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    proxy: { '/api': { target: 'http://localhost:3000', changeOrigin: true } }
   }
 })
