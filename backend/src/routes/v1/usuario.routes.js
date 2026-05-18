0622f1b0296da00adc3b4304c615b4542e469b17
Matias Xavier Roveri <matixr2008@gmail.com>
2026-05-17 21:30:33 -0300

    Add files via upload

---
+++ backend/src/routes/v1/usuario.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+import { usuarioController } from '../../controllers/usuario.controller.js'
+import { authMiddleware } from '../../middlewares/auth.js'
+
+const router = Router()
+router.use(authMiddleware.verifyToken)
+
+router.get('/perfil', usuarioController.getPerfil)
+router.put('/perfil', usuarioController.updatePerfil)
+
+export default router
