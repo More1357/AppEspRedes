2eb27855162ca0c853b38050bce2101dc72e44a6
Matias Xavier Roveri <matixr2008@gmail.com>
2026-05-17 21:21:40 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Inicio from './page/Inicio';
-
-console.log("✅ App.jsx se está ejecutando");
+import BibliotecaEjercicios from './page/BibliotecaEjercicios';
+import CalendarioSemanal from './page/CalendarioSemanal';
+import MisRutinas from './page/MisRutinas';
+import ConfigurarPerfil from './page/ConfigurarPerfil';
 
 function App() {
-  console.log("✅ Componente App renderizando");
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
+        <Route path="/biblioteca" element={<BibliotecaEjercicios />} />
+        <Route path="/calendario" element={<CalendarioSemanal />} />
+        <Route path="/mis-rutinas" element={<MisRutinas />} />
+        <Route path="/configurar-perfil" element={<ConfigurarPerfil />} />
       </Routes>
     </BrowserRouter>
   );
