29463806a17bc13c4cf97f61ee830dd5c8c05cb5
More1357 <randisimorena@gmail.com>
2026-06-14 22:09:09 -0300

    Update index.js

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -1,9 +1,12 @@
-import { Router } from 'express'
-import authRoutes from './auth.routes.js'
-import usuarioRoutes from './usuario.routes.js'
+import { Router } from 'express';
+import authRoutes from './auth.routes.js';
+import usuarioRoutes from './usuario.routes.js';
+import rutinaRoutes from './rutina.routes.js';
 
-const router = Router()
-router.use('/auth', authRoutes)
-router.use('/usuario', usuarioRoutes)
+const router = Router();
 
-export default router
+router.use('/auth', authRoutes);
+router.use('/usuario', usuarioRoutes);
+router.use('/rutinas', rutinaRoutes);
+
+export default router;
